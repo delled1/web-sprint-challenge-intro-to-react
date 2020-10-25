9ee0e8a789f1b2df8940d5c46a91c7cbc92511f6
eric <eric.della@yahoo.com>
2020-10-24 22:07:22 -0700

    import star wars api, started character cards

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import axios from 'axios';
+import React, {useEffect, useState} from 'react';
 import './App.css';
+import Character from "./components/Character"
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -9,9 +11,26 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+  const [character, getCharacter] = useState([])
+
+  useEffect(() => {
+
+    axios.get('https://swapi.dev/api/people/')
+    .then((response) => {
+      console.log(response.data)
+      getCharacter(response.data.results)
+      
+    })
+    .catch((error) =>
+    console.log(error))
+  }, [])
+
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      {character.map((character, index) => {
+        return <Character char={character} key={index}/>
+      })}
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,32 @@
 // Write your Character component here
+//ICE
+
+import React, { useState, useEffect} from 'react'
+
+function Character ({char, index})  {
+
+    return(
+        <div>
+            <h2>{char.name}</h2>
+            <p>
+                Birth Year: {char.birth_year}
+                <br></br>
+                Gender: {char.gender}
+                <br></br>
+                Height: {char.height}
+                <br></br>
+                Mass: {char.mass}
+                <br></br>
+                Eye Color: {char.eye_color}
+                <br></br>
+                Hair Color: {char.hair_color}
+                <br></br>
+                Skin Color: {char.skin_color}
+            </p>
+        </div>
+    )
+
+
+}
+
+export default Character
